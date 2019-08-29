fa7d31bc2fdd11afcdede1efd9e99aa3285b464a
Sergio Chamorro <schamorro@serlicorera.com>
2019-08-29 12:28:25 -0600

    Haciendo cambios menores

--- server.py
+++ server.py
@@ -1,20 +1,37 @@
 from flask import Flask, escape, request
 from flask import send_file
+from Graph.plot import Plot
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+   print(request.method)
+   req_data= request.get_json()
+   print(req_data)
+   name = request.args.get("name", "World")
+   return f'Hello, {escape(name)}!'
 
 
 
 
-@app.route('/get_image')
+@app.route('/get_image',methods=["POST"])
 def get_image():
-    if request.args.get('type') == '1':
-       filename = 'test.png'
-    else:
-       filename = 'test.png'
-    return send_file(filename, mimetype='image/png')
+   
+   req_data= request.get_json()
+   plot= Plot()
+   
+   plot.labels_x=list(req_data["labels_x"])
+   plot.labels_y=req_data["label_y"]
+   plot.title=req_data["title"]
+   plot.legend=list(req_data["legend"])
+   plot.valueGroup1=list(req_data["valueGroup"][0])
+   plot.valueGroup2=list(req_data["valueGroup"][1])
+   plot.filename=req_data["filename"]
+   if req_data["type"]=="1":
+      plot.createGroupBarPlot()
+   elif req_data["type"]=="2":
+      plot.createPieChart()
+
+   
+   return send_file(req_data["filename"], mimetype='image/png')
